f6892b7f5f2359dc5d47fff838314bf046ee193a
20AbhishekRoka02 <75659036+20AbhishekRoka02@users.noreply.github.com>
2025-10-06 23:04:42 +0530

    Update database.py for pre_ping and reconnect.

--- backend/auto-approve-bot/database.py
+++ backend/auto-approve-bot/database.py
@@ -11,5 +11,11 @@ DATABASE_URL=os.environ['DATABASE_URL']
 # sqlite_file_name = "schoolerp.db"
 # sqlite_url = f"sqlite:///./{sqlite_file_name}"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, 
+  echo=True,
+  pool_pre_ping=True,   # Auto-checks if connection is alive
+  pool_recycle=1800,    # Reconnect every 30 min to avoid timeout
+  pool_size=5,          # Controls how many connections are kept open
+  max_overflow=10      # Allows temporary extra connections
+  )
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
